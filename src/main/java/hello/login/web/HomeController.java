0877a157a3c845de818cc9fa9d1428acb83ca9fc
username <user@email.mail>
2022-05-11 16:32:23 +0900

    session last commit

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -61,7 +62,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String loginHomeV3(HttpServletRequest request, Model model){
 
        //세션이 없으면 home
@@ -72,6 +73,21 @@ public class HomeController {
 
         //로그인 시점에 세션에 보관한 회원 객체를 찾는다
         Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+        //세션에 회원 데이터가 없으면 home
+        if(loginMember == null){
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String loginHomeV3(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+            Model model){
+
         //세션에 회원 데이터가 없으면 home
         if(loginMember == null){
             return "home";
