1cc42cf0d2c20af2996436ae2b12be4a1fd923bf
username <user@email.mail>
2022-05-11 15:17:04 +0900

    session commit

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,12 +10,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
 //    @GetMapping("/")
     public String home() {
@@ -22,7 +27,7 @@ public class HomeController {
     }
 
     //required = false -> 로그인하지 않은 사용자도 홈에 접근할 수 있음
-    @GetMapping("/")
+//    @GetMapping("/")
     public String loginHome(@CookieValue(name = "memberId", required = false) Long memberId, Model model){
 
         //로그인 쿠키 없으면 기존 home으로
@@ -37,6 +42,42 @@ public class HomeController {
             return "home";
         }
 
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+//    @GetMapping("/")
+    public String loginHomeV2(HttpServletRequest request, Model model){
+
+        //세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
+
+        //로그인 쿠키가 있어도, 회원이 없으면 기존 home으로
+        if(member == null){
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String loginHomeV3(HttpServletRequest request, Model model){
+
+       //세션이 없으면 home
+        HttpSession session = request.getSession(false);
+        if(session == null){
+            return "home";
+        }
+
+        //로그인 시점에 세션에 보관한 회원 객체를 찾는다
+        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+        //세션에 회원 데이터가 없으면 home
+        if(loginMember == null){
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,9 @@
+package hello.login.web;
+
+public interface SessionConst {
+    String LOGIN_MEMBER = "loginMember";
+}
+
+//public class SessionConst{
+//    public static final String LOGIN_MEMBER = "loginMember";
+//}
